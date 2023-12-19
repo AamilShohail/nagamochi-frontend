6e6a3082d0c1fdc49ebb5b7321b6b836451e433c
Shohail Aamil <shohail@corzent.com>
2023-12-19 22:12:32 +0530

    footer and header

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
-export class AppComponent {
-  title = 'nagamochi-frontend';
+export class AppComponent implements OnInit {
+  title = 'Nagamochi Japanese Academy';
+
+  constructor(private titleService: Title) {}
+
+  ngOnInit(): void {
+    this.titleService.setTitle(this.title);
+  }
 }

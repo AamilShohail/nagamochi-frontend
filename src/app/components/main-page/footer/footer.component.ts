e2ca2c870dc84187da8be00c2612ea51365f26c6
Shohail Aamil <shohail@corzent.com>
2023-12-22 07:14:58 +0530

    modify header and footer

--- src/app/components/main-page/footer/footer.component.ts
+++ src/app/components/main-page/footer/footer.component.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { ContactDetails } from '@app/components/model/contact-details';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss',
 })
-export class FooterComponent {}
+export class FooterComponent {
+  contactDetails: ContactDetails = {
+    address: 'Kadahapola, Horambawa',
+    email: 'nagamochi.jacademy@gmail.com',
+    telephone: '+94 70 326 9897',
+  };
+}
---
+++ src/app/components/model/contact-details.ts
@@ -0,0 +1,5 @@
+export interface ContactDetails {
+  address: string;
+  email: string;
+  telephone: string;
+}
